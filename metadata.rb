dbd5fce80563117a01857885bcfee589ca86e4b3
Alex Bowen <bowen.a@gmail.com>
2013-06-28 22:22:27 -0400

    updated version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email 'https://github.com/trajar'
 license          'Apache 2.0'
 description      'Installs/Configures wordpress'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.8.8'
+version          '0.1.0'
 
 recipe 'wordpress', 'Installs and configures wordpress site(s) on a single system.'
 
